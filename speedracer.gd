2c0de21114f5336f3a955f921777c57ef576797a
Bonnez-s <guilhermedesouzamesquita1223@gmail.com>
2024-09-04 12:16:06 -0300

    4/9

--- speedracer.gd
+++ speedracer.gd
@@ -1,47 +1,40 @@
-extends Node2D
+extends CharacterBody2D
 
-var pos_0
 var speed=150
 var jump=0
-var rotate=0
 var jump_velo=400
+var high=0
+var floor=0
 
-func _ready():
-	pos_0=position.y
 	
-func _process(delta):
-	move(delta)
-	
-func move(delta):
-	position.x +=delta * speed
+func _physics_process(delta: float):
 	#JUMP#
+	
 	if Input.is_action_pressed("jump") and jump <21:
+		
 		jump +=1
 		position.y -= delta*jump_velo
-		rotate -=1
-		if rotate <=-12:
-			rotate=-12
-		$car.rotation =rotate*delta
-	#DOWN#
-	if Input.is_action_just_released("jump") or jump>=21:
-		if position.y>pos_0:
-			position.y += delta*0.8
-			rotate +=1
-			if rotate >=12:
-				rotate=12
-			$car.rotation =rotate *delta
+		high-=0.5
+		if high <=-3:
+			$car.position.y = -3
+			floor=0
 		else:
-			jump=0
-			position.y<=pos_0
-			rotate -=1
-			if rotate <=0:
-				rotate=0
-			$car.rotation =rotate*delta
-			
-	#ACELL/DESACELL#
-	if Input.is_action_pressed("acelerate"):
-		speed=8
-	if Input.is_action_pressed("desacelarete"):
-		speed=3
-	if Input.is_action_just_released("acelerate") or Input.is_action_just_released("desacelarete"):
-		speed=3
+			$car.position.y +=high*delta
+	#DOWN#
+	if floor==1:
+		jump=0
+		$car.position.y= 0
+		
+	else:
+		position.y += delta*100 
+		if (Input.is_action_just_released("jump") and jump>0 and jump <21) or jump==20 :
+			high+=0.5
+			if high >=3:
+				$car.position.y = 3
+				
+			else:
+				$car.position.y +=high*delta
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	floor=1
